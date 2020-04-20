ea88ce36a9745d2a24687919a6c51d2a247103e0
Dan Dickinson <daniel.dickison@austin.utexas.edu>
2020-04-20 14:07:13 -0500

    Tweak to eliminate TIFFStack warnings in windowMean.m

--- My Functions/windowMean.m
+++ My Functions/windowMean.m
@@ -7,12 +7,15 @@ function [avgImg] = windowMean(img, window)
 [ymax, xmax, tmax] = size(img);
 nMeans = floor(tmax/window); %Here floor is used to average only windows that fit evenly into the image length
 avgImg = zeros(ymax,xmax,nMeans);
+progress = waitbar(0,'Calculating windowed average (this may take a while)');
 for a = 1:nMeans
+    waitbar((a-1)/nMeans,progress);
     avgImg(:,:,a) = mean(img(:,:, ( (a-1)*window + 1 ) : a*window ), 3);
 end
+close(progress);
 
 if mod(tmax,window) ~= 0 %If there are extra frames, average them and append to the end of the average image
-    avgImg(:,:,end+1) = mean(img(:,:, (nMeans*window + 1):end));
+    avgImg(:,:,end+1) = mean(img(:,:, (nMeans*window + 1):end), 3);
 end
 
 end
