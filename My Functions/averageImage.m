7db0288629f1055c4499c836cc82a562596a1c47
Dan Dickinson <daniel.dickison@austin.utexas.edu>
2020-04-20 17:05:56 -0500

    Moved part of spotcout_ps to a separate function to allow reuse.

---
+++ My Functions/averageImage.m
@@ -0,0 +1,31 @@
+% Averages several frames of an image time series to produce a single 2d
+% image with higher signal:noise. The "params" input structure must at a
+% minimum include the following fielsds: 
+%   -firstTime (start of averaging window) 
+%   -lastTime (end of averaging window)
+%   -imageName (can be any string; for saving)
+
+function [avgImage] = averageImage(rawImage, channel, params)
+    [ymax, xmax, tmax] = size(rawImage);
+    
+    % Double check that our averaging window exists
+    if tmax <= params.lastTime    %If only a few frames were captured, use the whole timeseries 
+        params.firstTime = 1;
+        params.lastTime = tmax;
+    else                    %Otherwise, use the range specified in the input
+        if params.firstTime < 1 
+            params.firstTime = 1;
+        end
+        if params.lastTime > tmax
+            params.lastTime = tmax;
+        end
+    end
+    
+    %Make average image 
+    avgImage = uint16( mean( rawImage(:,:,params.firstTime:params.lastTime), 3) );
+    
+    %Save average image for later reference
+    imwrite(avgImage,[outStruct.tiffDir filesep params.imageName '_' channel 'avg.tif'],'tiff');
+
+end
+
