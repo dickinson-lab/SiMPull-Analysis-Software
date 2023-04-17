b08d10dc5c6586aac4fe4bde0aa44bcbf6feb95b
Daniel Dickinson <daniel.dickinson@austin.utexas.edu>
2023-04-17 16:47:52 -0700

    Fixed a bug in the dynamic channel info dialog box.

--- dynamic/detectCoAppearance.m
+++ dynamic/detectCoAppearance.m
@@ -83,6 +83,7 @@ elseif nargin == 3
     Answer = varargin{2};
     v2struct(Answer);
     regData = varargin{3};
+    DataType = 'Composite Data';  %% Data types are deprecated, set here to preserve backward compatibility
 else
     error('Incorrect number of input arguments given. Call detectCoAppearance(imgFilesCellArray, DialogBoxAnswers, regData) to provide parameters, or call with no arguments to raise dialog boxes.');
 end
