9f47faece4ad0a4b6bf7549ecdefda9056812dfb
Daniel Dickinson <daniel.dickinson@austin.utexas.edu>
2025-11-19 09:49:04 -0600

    Improved output of number of molecules counted from occupancyBySize.m

--- Dynamic_Analysis/Visualization/occupancyBySize.m
+++ Dynamic_Analysis/Visualization/occupancyBySize.m
@@ -6,36 +6,33 @@ matFiles = uipickfiles('Prompt','Select data files to analyze','Type',{'*.mat'})
 table = tabulateCoAppearance(matFiles);
 nFiles = length(matFiles);
 
-% Condense and Plot results
+%% Condense and Plot results
 for c = fieldnames(table.totalCounted) %This loops over prey channels   
     preyChannel = c{1};
-    pctCoApp = zeros(nFiles,1);
-    pctOcc = zeros(nFiles,1);
+    coApp = zeros(nFiles,5);
+    nPrey = zeros(nFiles,5);
+    countedSpots = zeros(nFiles,5);
+    countedMolecules = zeros(nFiles,5);
     % Calculate stats for complexes with 1-4 subunits separately
     for d = 1:4 
-        coApp = sum(table.totalCoApp.(preyChannel)(:,:,d),2,'omitnan');
-        nPrey = sum(table.total_nPrey.(preyChannel)(:,:,d),2,'omitnan');
-        counted = sum(table.totalCounted.(preyChannel)(:,:,d),2,'omitnan');
-        countedMolecules = d*counted;
-        pctCoApp(:,d) = 100 * (coApp ./ counted );
-        pctOcc(:,d) = 100 * (nPrey ./ (countedMolecules) );
-        disp(['N(' num2str(d) ')=' num2str(sum(counted))]);
+        coApp(:,d) = sum(table.totalCoApp.(preyChannel)(:,:,d),2,'omitnan');
+        nPrey(:,d) = sum(table.total_nPrey.(preyChannel)(:,:,d),2,'omitnan');
+        countedSpots(:,d) = sum(table.totalCounted.(preyChannel)(:,:,d),2,'omitnan');
+        countedMolecules(:,d) = d*countedSpots(:,d);
+        disp(['N(' num2str(d) ')=' num2str(sum(countedSpots(:,d)))]);
     end
     % Pool complexes with 5+ subunits since these are rarer
     [~,~,maxSize] = size(table.totalCoApp.(preyChannel));
-    coApp = zeros(nFiles,1);
-    nPrey = zeros(nFiles,1);
-    counted = zeros(nFiles,1);
-    countedMolecules = zeros(nFiles,1);
+
     for d = 5:maxSize
-        coApp = coApp + sum(table.totalCoApp.(preyChannel)(:,:,d),2,'omitnan');
-        nPrey = nPrey + sum(table.total_nPrey.(preyChannel)(:,:,d),2,'omitnan');
-        counted = counted + sum(table.totalCounted.(preyChannel)(:,:,d),2,'omitnan');
-        countedMolecules = countedMolecules + d*sum(table.totalCounted.(preyChannel)(:,:,d),2,'omitnan');
+        coApp(:,5) = coApp(:,5) + sum(table.totalCoApp.(preyChannel)(:,:,d),2,'omitnan');
+        nPrey(:,5) = nPrey(:,5) + sum(table.total_nPrey.(preyChannel)(:,:,d),2,'omitnan');
+        countedSpots(:,5) = countedSpots(:,5) + sum(table.totalCounted.(preyChannel)(:,:,d),2,'omitnan');
+        countedMolecules(:,5) = countedMolecules(:,5) + d*sum(table.totalCounted.(preyChannel)(:,:,d),2,'omitnan');
     end
-    pctCoApp(:,5) = 100 * (coApp ./ counted );
-    pctOcc(:,5) = 100 * (nPrey ./ countedMolecules );
-    disp(['N(5+)=' num2str(sum(counted))]);
+    pctCoApp = 100 * (coApp ./ countedSpots );
+    pctOcc = 100 * (nPrey ./ countedMolecules );
+    disp(['N(5+)=' num2str(sum(countedSpots(:,5)))]);
     % Plot
     plotSpreadBubble(pctCoApp,'showWeightedMean',true);
     title('Percent Co-Appearance');
