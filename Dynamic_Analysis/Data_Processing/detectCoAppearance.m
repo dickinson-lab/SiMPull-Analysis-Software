519f3fb6b54997cf521e9d41750fcc4fe1555ded
Dan Dickinson <daniel.dickinson@austin.utexsa.edu>
2026-08-17 15:50:48 -0500

    Further tweaking the definition of co-appearance to find true positives and ignore artifacts.

--- Dynamic_Analysis/Data_Processing/detectCoAppearance.m
+++ Dynamic_Analysis/Data_Processing/detectCoAppearance.m
@@ -395,12 +395,22 @@ function dynData = findCoApp(dynData, baitChannel, preyChannel, wb)
                 continue 
             end
             matchingStep = find( abs(baitAppearTime - preyStepTimes) <= 4 );  %Spots appearing within 4 frames of each other are considered simultaneous
+            % Retain only up-steps
+            if ~isempty(matchingStep)
+                idx_i = false(1,length(matchingStep));
+                for i = 1:length(matchingStep)
+                    if dynData.([preyChannel 'SpotData'])(c).steplevels(matchingStep(i)+1) <= dynData.([preyChannel 'SpotData'])(c).steplevels(matchingStep(i))
+                        idx_i(i) = true;
+                    end
+                end
+                matchingStep(idx_i) = [];
+            end
 
-            if ~isempty(matchingStep) && dynData.([preyChannel 'SpotData'])(c).steplevels(max(matchingStep)+1) > dynData.([preyChannel 'SpotData'])(c).steplevels(min(matchingStep)) % && the intensity has to increase (otherwise it's not an appearance event)
-                % We found a matching step; now make sure it's due to an actual increase in signal rather than bleaching of a neighboring spot.                                      % The "min" and "max" avoid crashing when more than one step matches.
+            if ~isempty(matchingStep)
+                % We found a matching step; now make sure it's due to an actual increase in signal rather than bleaching of a neighboring spot.                                      
                 % If it is, the signal will start close to zero and increase to a large positive value. Otherwise, it will start negative and increase to zero.
-                % So our heuristic is that if the level post-step is further from zero than the level pre-step, we consider it a true step. 
-                if abs(dynData.([preyChannel 'SpotData'])(c).steplevels(max(matchingStep)+1)) > abs(dynData.([preyChannel 'SpotData'])(c).steplevels(min(matchingStep)))
+                % So our heuristic is that if the level post-step is further from zero than the level pre-step, we consider it a true step.
+                if abs(dynData.([preyChannel 'SpotData'])(c).steplevels(max(matchingStep)+1)) > abs(dynData.([preyChannel 'SpotData'])(c).steplevels(min(matchingStep)))    % The "min" and "max" avoid crashing when more than one step matches.
                     dynData.([baitChannel 'SpotData'])(c).(['appears_w_' preyChannel]) = true;
                 else
                     dynData.([baitChannel 'SpotData'])(c).(['appears_w_' preyChannel]) = false;
